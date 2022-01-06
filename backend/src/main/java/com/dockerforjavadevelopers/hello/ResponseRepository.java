1f30dd160fc02101baddcd184fd96e207df9473c
mosieczek <ihatebisons@gmail.com>
2022-01-06 18:42:25 +0100

    dziala duzo rzeczy

--- backend/src/main/java/com/dockerforjavadevelopers/hello/ResponseRepository.java
+++ backend/src/main/java/com/dockerforjavadevelopers/hello/ResponseRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 @Repository
